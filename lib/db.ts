60de9e81d2bcf5bc4d6fb17f49aa9896d2cd37db
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-10-17 22:38:04 +0530

    fixed db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-// Extend the global object to include `prisma`
-declare global {
-  // You can also use `let` here as it's reassignable in development
-  // `PrismaClient | undefined` allows for lazy initialization in development mode
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient | undefined;
-    }
-  }
-}
-
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  // In development mode, prevent multiple instances of Prisma Client
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!(global as any).__prisma) {
+    (global as any).__prisma = new PrismaClient();
   }
-  prisma = global.prisma; // Assign the global instance
+  prisma = (global as any).__prisma;
 }
 
 export default prisma;
